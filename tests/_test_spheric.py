f72e027cfbd51a279c2e2fd1b33f99378f144889
Jorge Federico Bustillos Rava <108427440+FeD7791@users.noreply.github.com>
2023-10-03 15:06:29 -0300

    spheric test agregado

---
+++ tests/_test_spheric.py
@@ -0,0 +1,13 @@
+from sys import path
+import os 
+funciones_path = os.path.realpath('../voidfinder_tk')
+path.append(funciones_path)
+from finders import SphericalVoid
+
+def test_voidfinder():
+    test_void = SphericalVoid(radius=np.nan,x=np.nan,y=np.nan,z=np.nan,vx=np.nan,vy=np.nan,vz=np.nan,delta=np.nan,d_type=np.nan)
+    assert test_void.type_of_v() == 'No type'
+
+def test_voidfinder():
+    test_void = SphericalVoid(17.328250,445.838043,168.413986,84.131096,-148.989853,-135.213730,111.886261,-0.901690,-0.025397)
+    assert test_void.type_of_v() == 'R'
