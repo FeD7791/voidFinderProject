b2e7d77116d4428332010add504dbbd64d76c1df
Jorge Federico Bustillos Rava <108427440+FeD7791@users.noreply.github.com>
2023-10-03 14:53:12 -0300

    clase SphericalVoid agregada

--- voidfinder_tk/finders/spheric.py
+++ voidfinder_tk/finders/spheric.py
@@ -1,10 +1,28 @@
 # finders/spheric.py
+from typing import NamedTuple
 
-
-class Spheric:
-    def __init__(self, mesh="voronoi"):
-        self.mesh = mesh
-
-    def find(self, box):
-        # llamar al codigo del Polaco
-        pass
+class SphericalVoid(NamedTuple):
+    radius : float
+    x : float
+    y : float
+    z : float
+    vx : float
+    vy : float
+    vz : float
+    delta : float
+    d_type : float
+    
+    def type_of_v(self):
+        delta = self.d_type
+        _type_void : str
+        try:
+            if (delta>0):
+                _type_void = 'S'
+            elif (delta<0):
+                _type_void = 'R'
+            else:
+                return 'No type'
+        except:
+            print('Invalid delta')
+        else:
+            return _type_void
