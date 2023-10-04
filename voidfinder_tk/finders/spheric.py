0e052353a14ae6298aa1be4f20fde0eae7eb3157
Jorge Federico Bustillos Rava <108427440+FeD7791@users.noreply.github.com>
2023-10-04 16:21:32 -0300

    cambios de estilo (flake8) mas docstrings agregados

--- voidfinder_tk/finders/spheric.py
+++ voidfinder_tk/finders/spheric.py
@@ -1,28 +1,62 @@
-# finders/spheric.py
 from typing import NamedTuple
 
+
 class SphericalVoid(NamedTuple):
+
+    '''
+    Class definition for a Sperical Void Object
+
+    Parameters
+    ----------
     radius : float
-    x : float
-    y : float
-    z : float
-    vx : float
-    vy : float
-    vz : float
-    delta : float
-    d_type : float
-    
+        Radius of the Spherical Void
+    x : scalar(float)
+        x position of the center of the Spherical Void
+    y : scalar(float)
+        y position of the center of the Spherical Void
+    z : scalar(float)
+        z position of the center of the Spherical Void
+    vx : scalar(float)
+        medium velocity of the void in the x direction
+    vy : scalar(float)
+        medium velocity of the void in the y direction
+    vz : scalar(float)
+        medium velocity of the void in the z direction
+    d_type : scalar(float)
+        density for radius > radius void
+    '''
+    radius: float
+    x: float
+    y: float
+    z: float
+    vx: float
+    vy: float
+    vz: float
+    d_type: float
+
     def type_of_v(self):
-        delta = self.d_type
-        _type_void : str
+        '''
+        Function used to calculate the type of the void. Types could
+        be: S (High density profile) , R (Low density profile).
+
+        Returns
+        -------
+        type_void : string
+            S : High density profile void
+            R : Low density profile void
+
+
+        '''  
+        _delta = self.d_type
+        type_void: str
         try:
-            if (delta>0):
-                _type_void = 'S'
-            elif (delta<0):
-                _type_void = 'R'
+            if (_delta > 0):
+                type_void = 'S'
+            elif (_delta < 0):
+                type_void = 'R'
             else:
-                return 'No type'
-        except:
+                return 'No type'   
+        except (TypeError):
             print('Invalid delta')
         else:
-            return _type_void
+            return type_void
