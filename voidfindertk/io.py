62775f79cade2c67cf89708ed68bec1f4ae0d285
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2023-10-23 13:08:18 -0300

    fix: if condition, rebuild data and box

--- voidfindertk/io.py
+++ voidfindertk/io.py
@@ -17,25 +17,26 @@ def read_table(input_path,**kwargs):
         List of [x,y,z,vx,vy,vz,m] that references to columns with the 
         needed input
     """
-    path = f"{input_path}"
+    path = input_path
     keys_set = {'x','y','z','vx','vy','vz','m'}
 
    
     if kwargs:
-        if ('x' or 'y' or 'z'or 'vx' 
-            or 'vy' or 'vz' or 'm' not in kwargs):
+        if (not (all(key in kwargs for key in keys_set))): 
             
             missing_variables = keys_set.difference(set(kwargs.keys()))
             raise MissingValuesError('Missing the following values:',missing_variables) 
-        else:
-            data_cols = [
-            kwargs['x'],kwargs['y'],kwargs['z'],
-            kwargs['vx'],kwargs['vy'],kwargs['vz'],
-            kwargs['m'] ]
         
+        data_cols = [
+        kwargs['x'],kwargs['y'],kwargs['z'],
+        kwargs['vx'],kwargs['vy'],kwargs['vz'],
+        kwargs['m'] ]
         
-        data = pd.read_table(path, sep="\s+",usecols=data_cols)
-        box = Box(
+    else:
+        data_cols = [0,1,2,3,4,5,6]
+        
+    data = pd.read_table(path, sep="\s+",usecols=data_cols) 
+    box = Box(
         data.iloc[:,0],
         data.iloc[:,1],
         data.iloc[:,2],
@@ -44,21 +45,7 @@ def read_table(input_path,**kwargs):
         data.iloc[:,5],
         data.iloc[:,6]
         )
-        return box
-        
-    else:
-        data_cols = [0,1,2,3,4,5,6]
-        data = pd.read_table(path, sep="\s+",usecols=data_cols)
-        box = Box(
-        data.iloc[:,0],
-            data.iloc[:,1],
-            data.iloc[:,2],
-            data.iloc[:,3],
-            data.iloc[:,4],
-            data.iloc[:,5],
-            data.iloc[:,6]
-        )
-        return box
+    return box
         
 
     
