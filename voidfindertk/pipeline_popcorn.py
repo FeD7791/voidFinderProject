08e408e4fe63a200ded50bfb9eeffb5ab36c3d1e
srgualpa <srgualpa@unc.edu.ar>
2024-03-26 10:39:24 -0300

    fix: Imprimir la sparse

--- voidfindertk/pipeline_popcorn.py
+++ voidfindertk/pipeline_popcorn.py
@@ -5,4 +5,4 @@ import numpy as np
 filename='../../../popcorn_202403/pocorn4/popcorn/MWE/clean_popvds.dat'
 datos = read_popcorn_output_to_dictionary(filename)
 sparse_matrix = create_sparse_matrix_from_dict(datos,700070)
-print(sparse_matrix.nonzero())
+print(sparse_matrix)
