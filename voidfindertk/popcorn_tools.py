c0b19dbb3776c07573462d1646cd6f0aee223e9e
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-03-29 20:37:49 -0300

    fix: removed object that combines box + void

---
+++ voidfindertk/popcorn_tools.py
@@ -0,0 +1,80 @@
+import uttr
+import numpy as np
+from .models import ModelABC
+from .analysis_tools import join_box_void
+
+
+@uttr.s(repr=False)
+class PopCornVoids:
+    void_id = uttr.ib()
+    number_members = uttr.ib()
+    void_volume = uttr.ib(converter=np.array)
+    number_subhalos = uttr.ib() 
+    internal_flag = uttr.ib()
+    spheres = uttr.ib()
+    ID_subhalo = uttr.ib()
+    _void_len = uttr.ib(init=False)
+
+    def __attrs_post_init__(self):
+        """Post init method.
+
+        Checks that the lenght of the inputs are the same
+        """
+        lengths = set(())
+        for e in (
+            self.void_id,
+            self.number_members,
+            self.void_volume ,
+            self.number_subhalos,
+            self.internal_flag,
+            self.spheres,
+            self.ID_subhalo
+        ):
+            lengths.add(len(e))
+
+        if len(lengths) != 1:
+            raise ValueError("Arrays should be of the same size")
+
+        super().__setattr__("_void_len", lengths.pop())
+
+    def __len__(self):
+        """Length method.
+
+        Returns
+        -------
+            int
+                the number of elements in SphericalVoids
+        """
+        return self._void_len
+    
+    def __repr__(self):
+        """Representation method.
+
+        Returns
+        -------
+            str
+                Name plus number of points in the box
+        """
+        cls_name = type(self).__name__
+        length = len(self)
+        return f"<{cls_name} size={length}>"
+    
+
+class PopCornVF(ModelABC):
+    def __init__(self):
+        pass
+
+    def preprocess(self, databox):
+        return databox
+
+    def model_find(self, llbox):
+        None #Not implemented yet
+        # sp_void = popcorn_void_finder(llbox.box) #This part calls the c method
+        # return {'voids':sp_void}
+    
+    def mk_vbox(self, databox,voids,sparse,llbox):
+        voids = voids['voids']
+        sparse_m = join_box_void(databox,voids,tol=0.0)
+        return sparse_m
+
+
--- voidfindertk/spherical_tools.py
+++ voidfindertk/spherical_tools.py
@@ -1,14 +1,10 @@
 from astropy import units as u
 import numpy as np
 import uttr
-from abc import ABC
 import os
 import ctypes
-import scipy
-from attrs import define
-from .box import Box
 from .models import ModelABC
-
+from .analysis_tools import join_box_void
 
 class SphericalVF(ModelABC):
     def __init__(self):
@@ -21,12 +17,12 @@ class SphericalVF(ModelABC):
         sp_void = spherical_void_finder(llbox.box)
         return {'voids':sp_void}
     
-    def mk_vbox(self, databox,voids,llbox):
+    def mk_vbox(self, databox,voids,sparse,llbox):
         voids = voids['voids']
-        databox.box.__dict__.pop('_len')
-        voids.__dict__.pop('_void_len')
+        # databox.box.__dict__.pop('_len')
+        # voids.__dict__.pop('_void_len')
         #sparse_m = Classifier(**databox.box.__dict__, **voids.__dict__)._sparse_matrix(0.0)
-        sparse_m = Classifier(**databox.box.__dict__, **voids.__dict__)
+        sparse_m = join_box_void(databox,voids,tol=0.0)
         return sparse_m
 
 
@@ -100,6 +96,7 @@ class SphericalVoids:
             raise ValueError("Arrays should be of the same size")
 
         super().__setattr__("_void_len", lengths.pop())
+
 
     def __len__(self):
         """Length method.
@@ -273,101 +270,6 @@ def spherical_void_finder(box):
 
     return sp_void
 
-@define(repr=False)
-class Classifier(Box, SphericalVoids):
-    """Inherits from both Box and ShpericalVoids classes and provides a method
-    to create a sparse matrix representing the relationship between spherical
-    voids and tracer particles within a simulation box.
 
-    Methods
-    -------
-    _sparse_matrix(self, tol=0.0)
-        Generates a sparse CSR matrix indicating which voids are potentially
-        influencing each tracer particle based on their relative positions.
-
-    Parameters
-    ----------
-    self : Classifier object
-        The instance of the class.
-    tol : float, optional
-        Tolerance level for considering a void to be influencing a tracer
-        particle. Defaults to 0.0.
-
-    Returns
-    -------
-    scipy.sparse.csr_matrix
-        A sparse CSR matrix where each row represents a tracer particle and
-        each column represents a void. The value at a specific row-column
-        intersection is 1.0 if the corresponding void is potentially
-        influencing the tracer particle (Tracer inside the void) based on
-        the distance between their centers and the void's radius,
-        otherwise 0.0.
-    """
-
-    def _sparse_matrix(self, tol=0.0):
-        tolerance = tol * np.ones(self.rad.shape)
-        rad = np.array(self.rad) + tolerance
-        pos_void = np.array([self.x_void, self.y_void, self.z_void])
-        pos_void = np.transpose(pos_void)
-        arr = []
-        for i in range(self._len):
-            pos_box = np.array(
-                [[self.x[i].value, self.y[i].value, self.z[i].value]]
-            )
-            d = scipy.spatial.distance.cdist(
-                pos_box, pos_void, metric="euclidean"
-            )  # Calculates the distance from a tracer to each void center
-            rad_center_distance_comparing = np.greater(rad, d[0]).astype(
-                float
-            )  # Compares radius of a void and distance from the void center to the particle 1 if rad>d # noqa: E501
-            arr.append(rad_center_distance_comparing)
-        # Transform to sparse matrix
-        output_sparse_matrix = scipy.sparse.csr_matrix(arr)
-        return output_sparse_matrix
-    
-    def _get_voids(self):
-            
-        
-        return {
-            'rad':self.rad,
-            'x_void':self.x_void,
-            'y_void':self.y_void,
-            'z_void':self.z_void,
-            'vel_x_void':self.vel_x_void,
-            'vel_y_void':self.vel_y_void,
-            'vel_z_void':self.vel_z_void,
-            'delta':self.delta,
-            'dtype':self.dtype,
-            'poisson':self.poisson,
-            'nran':self.nran
-            
-            }
-            
-    def _get_box(self):
-        return{
-        'x':self.x,
-        'y':self.y,
-        'z':self.z,
-        'vx':self.vx,
-        'vy':self.vy,
-        'vz':self.vz,
-        'm':self.m
-        }
-
-    def _get_type(self):
-        return 'SphericalVoids'
-
-    def __repr__(self):
-        """Representation method.
-
-        Returns
-        -------
-            str
-                Size of Box and calculated Voids
-        """
-        cls_name = type(self).__name__
-        # length_box = self._len
-        # length_voids = self._void_len
-        return f"<{cls_name} >"
         
 
