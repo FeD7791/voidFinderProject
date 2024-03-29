6e626bc7ca814db92e974c8d53e579ea1e04411f
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-03-29 20:41:54 -0300

    fix: Changed pipeline_popcorn file to update how the code is used

--- pipeline_popcorn.py
+++ pipeline_popcorn.py
@@ -1,47 +1,21 @@
-from voidfindertk.io_popcorn import read_popcorn_output_to_dictionary
-from voidfindertk.io_popcorn import create_sparse_matrix_from_dict
+
 from voidfindertk import io
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+from voidfindertk.analysis_tools import analysis_voids,join_box_void
 filename='./clean_popvds.dat'
-datos = read_popcorn_output_to_dictionary(filename)
+void = io.read_popcorn_output(filename)
+box = io.read_table('halos_fede.dat') 
 
-#Define params
-n_items = 500
-param = 'void_volume'
+void_out = join_box_void(box=box,voids=void)
+output = analysis_voids(**void_out,n_items=100)
+print(output.keys())
 
-#Define df_voids
-voids_list = list(datos['voids'])
-voids_all = []
-for i in voids_list:
-	voids_all.append(datos['voids'][f'{i}'])
-df_voids = pd.DataFrame.from_dict(voids_all)
-
-#Define sparse
-sparse = create_sparse_matrix_from_dict(datos,700070).transpose()
-	
-#Define box
-df_box = pd.DataFrame(io.read_table('./halos_fede.dat').__dict__)
-
-#OPeration
-df_filtered_param = df_voids.sort_values(by=param, ascending=False)
-df_filtered_index_cut = df_filtered_param.index[:n_items]
-velocity_voids = []
-for i in df_filtered_index_cut:
-	tracer_indexes = list(sparse.getcol(i).tocoo().row)
-	velocity_voids.append({
-	'void':i, 
-	'velocity_norm':np.linalg.norm(df_box.loc[tracer_indexes,['vx','vy','vz']].sum(axis=0))/len(tracer_indexes),
-  	'n_tracers':len(tracer_indexes) 
-	})
-
-velocity_voids_df = pd.DataFrame(velocity_voids)
-sns.histplot(data=velocity_voids_df, x='velocity_norm', bins=50, kde=True).set(title=f'nvoids:{len(velocity_voids)}')
+sns.histplot(data=pd.DataFrame(output), x='velocity_norm', bins=50, kde=True).set(title=f'nvoids:{len(output['velocity_norm'])}')
 #sns.histplot(data=velocity_voids_df, x='n_tracers', bins=50, kde=True).set(title=f'nvoids:{len(velocity_voids)}')
 
-plt.savefig('popcorn_velocity_hist.jpg')
-#plt.savefig('popcorn_tracers_hist.jpg')
+# #plt.savefig('popcorn_velocity_hist.jpg')
+# #plt.savefig('popcorn_tracers_hist.jpg')
 plt.show()
