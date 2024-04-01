6ed8a1014a4c317acd4b170ceb731ce42396af33
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-04-01 02:08:34 -0300

    fix: adding two paths strings as inputs from python

--- voidfindertk/zobov/zobov_loader.c
+++ voidfindertk/zobov/zobov_loader.c
@@ -23,16 +23,20 @@ void c_binary_writter(
     double *arr_vx,
     double *arr_vy,
     double *arr_vz,
-    double *arr_m , int size
+    double *arr_m , int size,
+    char *raw_path, char *txt_path
 ){
+    // puts(raw_path);
+    // puts(txt_path);
     
-    FILE *fout = fopen("tracers_zobov.raw","wb");
+    FILE *fout = fopen(raw_path,"wb");
     fwrite(&size,sizeof(int),1,fout);
     fwrite(arr_x,sizeof(double),size,fout);
     fwrite(arr_y,sizeof(double),size,fout);
     fwrite(arr_z,sizeof(double),size,fout);
     fclose(fout);
-    FILE *fout2 = fopen("tracers_zobov.txt","w");
+    
+    FILE *fout2 = fopen(txt_path,"w");
     fprintf(fout2, "%d\n", size);
     int i;
     for (i = 0; i < size; i++) {
