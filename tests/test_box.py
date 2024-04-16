e5f318c72fbe69e4fda6fee9d35dc65923ed5c21
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-04-16 11:01:52 -0300

    tests: modified test box

--- tests/test_box.py
+++ tests/test_box.py
@@ -14,8 +14,8 @@ from voidfindertk.box import Box
 
 
 def test_Box_initialization(mkbox):
-    box = mkbox(seed=42, size=1000)
-
+    box = mkbox(seed=42, size=1000)# mkbox inmediatamente retorna _maker(**kwargs)
+    #mkbox(seed=42, size=1000) --> _maker(**kwargs)
     assert box.x.unit == u.Mpc
     assert box.y.unit == u.Mpc
     assert box.z.unit == u.Mpc
@@ -34,19 +34,23 @@ def test_Box_initialization(mkbox):
     assert isinstance(box.m, np.ndarray)
 
 
-def test_Box_different_length_for_some_dimension(mkbox_params):
+def test_Box_different_length_tracers(mkbox_params):
     params = mkbox_params(seed=42, size=1000)
     params["x"] = params["x"][:-1]
 
     with pytest.raises(ValueError, match="Arrays should be of the same size"):
         Box(**params)
 
+def test_Box_different_length_side_box(mkbox_params):
+    params = mkbox_params(seed=43, size=1000) #for this seed math.ceil(max(box.x)) = 499 instead default vaule of 500
+    with pytest.raises(ValueError, match="Not a cube: xmax: 499 ymax: 500 zmax: 500"):
+        Box(**params)
+
 
 def test_box_equality(mkbox):
     box1 = mkbox(seed=42, size=1000)
     box2 = mkbox(seed=42, size=1000)
-    box3 = mkbox(seed=43, size=1000)
-    box4 = mkbox(seed=42, size=999)
+    box3 = mkbox(seed=42, size=999)
     assert box1 == box2
     assert not box1 == box3
-    assert not box4 == box1
+    
