379bb8fb3583021fa5cf9c269d40b45aa5149d33
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-05-06 22:34:10 -0300

    refactor: zobov finder methods in zobov_void_finder method

--- voidfindertk/zobov_tools.py
+++ voidfindertk/zobov_tools.py
@@ -1,13 +1,75 @@
 import uttr
 import numpy as np
 from astropy import units as u
+import pandas as pd
 import os
 import ctypes
 import subprocess
 import os
 import shutil
+from .models import ModelABC
+from .analysis_tools import join_box_void
 
+class ZobovVF(ModelABC):
+    def __init__(self):
+        pass
 
+    def preprocess(self, databox):
+        '''
+            This part will create a couple of files: 
+            tracers_zobov.raw (binary file) and 
+            tracers_zobov.dat a csv file
+            tracers_zobov.raw is needed to feed zobov's
+            finder input and make it work. To create the files
+            a c script is used , it just takes the box reads it then
+            saves it in the binary file. This method will return databox
+            (which contains the box) as a dummy return (cause it will not be
+            used for anything)
+        '''
+        #Create input binary files for Zobov finder
+        path = os.path.dirname(os.path.realpath(__file__))
+        path_zobov = os.path.join(path,'zobov')
+
+        #Declare library path
+        clibrary = ctypes.CDLL(
+            os.path.join(path,'zobov','zobov_loader.so') ,
+            mode=ctypes.RTLD_GLOBAL
+            ) 
+
+        #Create Input Pointers for x,y,z,vx,vz,vy,m
+        arr_pointer = 7*[
+            np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, 
+                                   flags=['CONTIGUOUS'])]
+
+        #Declare Input Pointers type
+        clibrary.c_binary_writter.argtypes = arr_pointer + [
+            ctypes.c_int,ctypes.c_char_p, ctypes.c_char_p]
+        #Fill Input
+        clibrary.c_binary_writter(
+            databox.box.x,
+            databox.box.y,
+            databox.box.z,
+            databox.box.vx,
+            databox.box.vy,
+            databox.box.vz,
+            databox.box.m,
+            len(databox.box),
+            os.path.join(path_zobov,'tracers_zobov.raw').encode('utf-8'),
+            os.path.join(path_zobov,'tracers_zobov.txt').encode('utf-8')
+                            )
+        return databox
+
+    def model_find(self, llbox):
+        sp_void = zobov_void_finder(llbox.box)
+        return {'voids':sp_void}
+    
+    def mk_vbox(self, voids,llbox):
+        voids = voids['voids']
+        # databox.box.__dict__.pop('_len')
+        # voids.__dict__.pop('_void_len')
+        #sparse_m = Classifier(**databox.box.__dict__, **voids.__dict__)._sparse_matrix(0.0)
+        box_void_sparse = join_box_void(llbox.box, voids, tol=0.0)
+        return box_void_sparse
 
 @uttr.s(repr=False)
 class ZobovVoids:
@@ -60,21 +122,21 @@ class ZobovVoids:
         """
         return self._void_len
     
-    def _slice(self,min,max):
-        zobovvoid = {
-            'Void_number':self.Void_number[min:max],
-            'File_void_number':self.File_void_number[min:max],
-            'CoreParticle':self.CoreParticle[min:max],
-            'CoreDens':self.CoreDens[min:max],
-            'ZoneVol':self.ZoneVol[min:max],
-            'Zone_number_part':self.Zone_number_part[min:max],
-            'Void_number_Zones':self.Void_number_Zones[min:max],
-            'VoidVol':self.VoidVol[min:max],
-            'Void_number_Part':self.Void_number_Part[min:max],
-            'VoidDensContrast':self.VoidDensContrast[min:max],
-            'VoidProb':self.VoidProb[min:max]
-        }
-        return ZobovVoids(**zobovvoid)
+    # def _slice(self,min,max):
+    #     zobovvoid = {
+    #         'Void_number':self.Void_number[min:max],
+    #         'File_void_number':self.File_void_number[min:max],
+    #         'CoreParticle':self.CoreParticle[min:max],
+    #         'CoreDens':self.CoreDens[min:max],
+    #         'ZoneVol':self.ZoneVol[min:max],
+    #         'Zone_number_part':self.Zone_number_part[min:max],
+    #         'Void_number_Zones':self.Void_number_Zones[min:max],
+    #         'VoidVol':self.VoidVol[min:max],
+    #         'Void_number_Part':self.Void_number_Part[min:max],
+    #         'VoidDensContrast':self.VoidDensContrast[min:max],
+    #         'VoidProb':self.VoidProb[min:max]
+    #     }
+    #     return ZobovVoids(**zobovvoid)
     
     def __repr__(self):
         """Representation method.
@@ -88,59 +150,80 @@ class ZobovVoids:
         length = len(self)
         return f"<{cls_name} size={length}>"
     
-def zobov_void_finder(box):
-	path = os.path.dirname(os.path.realpath(__file__))
-	path_zobov = os.path.join(path,'zobov')
-	
-	#Declaramos la ubicacion de la libreria
-	clibrary = ctypes.CDLL(os.path.join(path,'zobov','zobov_loader.so') ,mode=ctypes.RTLD_GLOBAL) 
-
-	#Create Pointer
-	arr_pointer_x = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_y = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_z = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_vx = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_vy = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_vz = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
-	arr_pointer_m = np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags=['CONTIGUOUS'])
+def read_zobov_output(filename):
+    output = pd.read_csv(filename, sep='\s+', skiprows=2)
+    output.columns = [
+        'Void_number',
+        'File_void_number',
+        'CoreParticle',
+        'CoreDens',
+        'ZoneVol',
+        'Zone_number_part',
+        'Void_number_Zones',
+        'VoidVol',
+        'Void_number_Part',
+        'VoidDensContrast',
+        'VoidProb'
+    ]
+    zobov = ZobovVoids(**output.to_dict(orient='list'))
+    return zobov
     
 
-	#Declare Input Pointers
-	clibrary.c_binary_writter.argtypes = [
-	    arr_pointer_x,arr_pointer_y,arr_pointer_z,
-	    arr_pointer_vx,arr_pointer_vy,arr_pointer_vz,arr_pointer_m,ctypes.c_int,
-        ctypes.c_char_p, ctypes.c_char_p]
-	#Use function c_binary_writter
-	clibrary.c_binary_writter(box.x,box.y,box.z,box.vx,box.vy,box.vz,box.m,len(box),
-                os.path.join(path_zobov,'tracers_zobov.raw').encode('utf-8'),
-                os.path.join(path_zobov,'tracers_zobov.txt').encode('utf-8')
-                           )
+
+def zobov_void_finder(box,**kwargs):
+    #Params
+    kwargs.setdefault('buffer_size', 0.08)
+    kwargs.setdefault('box_size', box.size())
+    kwargs.setdefault('number_of_divisions',2)
+    kwargs.setdefault('delete_files', True)
+    kwargs.setdefault('density_threshold', 0.2)
+
+    #Paths
+    path = os.path.dirname(os.path.realpath(__file__))
+    path_zobov = os.path.join(path,'zobov')
+    
 
 
+    path_src = os.path.join(path_zobov,'src')
+    shutil.move(os.path.join(path_zobov,'tracers_zobov.raw'),path_src)
+    os.chdir(path_src)
 
+    #Runing Zobov 
+    subprocess.run(["./vozinit", "tracers_zobov.raw", 
+                    str(kwargs['buffer_size']), 
+                    str(kwargs['box_size']),
+                    str(kwargs['number_of_divisions']),
+                    'output_vozinit'])
+    subprocess.run(["./scroutput_vozinit"])
+    subprocess.run(["./jozov", 
+                    "adjoutput_vozinit.dat", 
+                    "voloutput_vozinit.dat", 
+                    'out_particle_zone.dat',
+                    'out_zones_in_void.dat',
+                    'out_text_file.dat',
+                    str(kwargs['density_threshold'])])
 
-	
-	
-	path_src = os.path.join(path_zobov,'src')
-	shutil.move(os.path.join(path_zobov,'tracers_zobov.raw'),path_src)
-	os.chdir(path_src)
-	#subprocess.Popen(["./vozinit", "tracers_zobov.raw", '0.08', '500','2','output_vozinit'], stdin=subprocess.PIPE)
-	subprocess.run(["./vozinit", "tracers_zobov.raw", '0.08', '500','2','output_vozinit'])
-	subprocess.run(["./scroutput_vozinit"])
-	subprocess.run(["./jozov", "adjoutput_vozinit.dat", "voloutput_vozinit.dat", 'out_particle_zone.dat','out_zones_in_void.dat','out_text_file.dat','0.2'])
-
-
-
-	#Delete unused files
-	subprocess.Popen(('find', '.', '-type', 'f', '-name', 'part.output_vozinit*', '-exec', 'rm','{}', ';')) #Delete part... files
-	#Delete other binary unused files
-
-	files_to_remove = ['tracers_zobov.raw',"adjoutput_vozinit.dat","voloutput_vozinit.dat",'out_particle_zone.dat','out_zones_in_void.dat','scroutput_vozinit']
-	for f in files_to_remove:
-		try:
-			os.remove(f)
-		except FileNotFoundError:
-			print(f'File {f} not found')
-	#Output Results
-               
+    #Delete unused files
+    if kwargs['delete_files']: #provide delete_files as false to preserve files
+        subprocess.Popen((
+            'find', '.', 
+            '-type', 'f', 
+            '-name', 'part.output_vozinit*', 
+            '-exec', 'rm','{}', ';')) #Delete part... files
+        #Delete other binary unused files
+        files_to_remove = [
+            'tracers_zobov.raw',
+            "adjoutput_vozinit.dat",
+            "voloutput_vozinit.dat",
+            'out_particle_zone.dat',
+            'out_zones_in_void.dat',
+            'scroutput_vozinit']
+        for f in files_to_remove:
+            try:
+                os.remove(f)
+            except FileNotFoundError:
+                print(f'File {f} not found')
+    #Output Results
+    zobov_voids = read_zobov_output(os.path.join(path_src,'out_text_file.dat'))   
+    return zobov_voids     
 
