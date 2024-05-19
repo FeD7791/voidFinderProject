3ed797cd142d7623ccf4714868a770b17e055ea5
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-05-19 18:28:54 -0300

    feat: adding VoidMetrics class and changing model find method

--- voidfindertk/models.py
+++ voidfindertk/models.py
@@ -1,17 +1,32 @@
 import abc
+from attrs import define,field
 from abc import abstractmethod, ABC
 from . import data_box
+
+@define 
+class VoidMetrics():
+    void_box_coordinates = field(init=False)
+    void_mass = field(init=False)
+    tracers = field(init=False)
+    voids = field(init=False)
 
 
 class ModelABC(ABC):
 
     def __init__(self):
         pass
-    def find(self,databox):
-        llbox = self.preprocess(databox)
+    def find(self,box):
+        llbox = self.preprocess(box)
         voids = self.model_find(llbox)
         vb = self.mk_vbox(voids,llbox)
-        return vb
+        mass = self.get_void_mass(voids,llbox)
+        metrics = VoidMetrics(**{
+            'void_box_coordinates':vb,
+            'void_mass':mass,
+            'tracers':llbox.box,
+            'voids':voids
+        })
+        return metrics
 
     @abstractmethod
     def preprocess(self, databox):
@@ -21,5 +36,8 @@ class ModelABC(ABC):
         pass
     @abstractmethod
     def mk_vbox(self,voids,llbox):
+        pass
+    @abstractmethod
+    def get_void_mass(self,void_box,llbox):
         pass
 
