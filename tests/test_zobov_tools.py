c4bc1a4d8884405e9c0da72aaee00bba04919fce
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2024-05-19 18:27:42 -0300

    tests: adding test for zobov_tools regarding to ZobovVF model

--- tests/test_zobov_tools.py
+++ tests/test_zobov_tools.py
@@ -1,9 +1,10 @@
 import numpy as np
 import scipy as sp
 from voidfindertk import box as b
-from voidfindertk.zobovvf.zobov_tools import ZobovVoids, calculate_tracers_inside_void
-
+from voidfindertk.zobovvf.zobov_tools import ZobovVoids,ZobovVF ,calculate_tracers_inside_void
+from voidfindertk import data_box
 ##Run just one pytest tests/test_zobov_tools.py -k 'test_calculate_tracers_inside_void'
+##Run just one pytest tests/test_zobov_tools.py -k 'test_ZobovVF'
 
 def test_calculate_tracers_inside_void(mkbox, make_zobov_voids_params):
     box = mkbox(seed=42, size=1000) #Make a box
@@ -21,3 +22,24 @@ def test_calculate_tracers_inside_void(mkbox, make_zobov_voids_params):
     voids.Void_number_Part[1] = 3
     #5 Closest particle of tracer 0 are [1,2,3,4,5] y closest 3 to tracer 9 are [8,7,6] 
     assert calculate_tracers_inside_void(box,voids) == [[1,2,3,4,5],[998,997,996]]
+
+def test_ZobovVF(mkbox, make_zobov_voids_params):
+    model = ZobovVF()
+    box = mkbox(seed=42, size=1000) #Make a box
+    dbox = data_box.DataBox(box)
+    params = make_zobov_voids_params(n_voids=100,CoreParticle=len(dbox.box)) #Create params for zobov
+    
+    #model_find step
+    voids = ZobovVoids(**params) #Create voids
+    voids._tracers_in_void = calculate_tracers_inside_void(dbox.box,voids)
+    voids = {'voids':voids}
+    ### End model_find step
+    #Test mk_vbox
+    assert model.mk_vbox(voids,dbox).shape == (len(dbox.box), len(voids['voids'])) 
+    #Test get_void_mass
+    #sum(voids._tracers_in_void,[]) transform list of list into one list
+    index = sum(voids['voids']._tracers_in_void,[])
+    a = sum(model.get_void_mass(voids=voids['voids'],llbox=dbox))
+    b = sum(box.m.value[index])
+    #Round result cause some precision is lost with dataframe
+    assert round(a,6) == round(b,6)
