2b5f57fa44a087128b41bfd19ad00a55d5cb1636
jorgefederico <jorgefedericobustillosrava@gmail.com>
2024-06-16 17:10:49 -0300

    reafactor: keeping DataBox class

--- voidfindertk/models.py
+++ voidfindertk/models.py
@@ -4,19 +4,18 @@ from abc import ABC, abstractmethod
 from attrs import define, field
 
 
+class DataBox:
+    box = None
+
+    def __init__(self, aux_box):
+        self.box = aux_box
+
 @define
 class VoidMetrics:
     void_box_coordinates = field(init=False)
     void_mass = field(init=False)
     tracers = field(init=False)
     voids = field(init=False)
-
-
-class DataBox:
-    box = None
-
-    def __init__(self, aux_box):
-        self.box = aux_box
 
 
 class ModelABC(ABC):
@@ -27,17 +26,17 @@ class ModelABC(ABC):
     def find(self, box):
         llbox = self.preprocess(box)
         voids = self.model_find(llbox)
-        vb = self.mk_vbox(voids, llbox)
-        mass = self.get_void_mass(voids, llbox)
-        metrics = VoidMetrics(
-            **{
-                "void_box_coordinates": vb,
-                "void_mass": mass,
-                "tracers": llbox.box,
-                "voids": voids,
-            }
-        )
-        return metrics
+        # vb = self.mk_vbox(voids, llbox)
+        # mass = self.get_void_mass(voids, llbox)
+        # metrics = VoidMetrics(
+        #     **{
+        #         "void_box_coordinates": vb,
+        #         "void_mass": mass,
+        #         "tracers": llbox.box,
+        #         "voids": voids,
+        #     }
+        # )
+        # return metrics
 
     @abstractmethod
     def preprocess(self, databox):
@@ -47,10 +46,10 @@ class ModelABC(ABC):
     def model_find(self, llbox):
         pass
 
-    @abstractmethod
-    def mk_vbox(self, voids, llbox):
-        pass
+    # @abstractmethod
+    # def mk_vbox(self, voids, llbox):
+    #     pass
 
-    @abstractmethod
-    def get_void_mass(self, void_box, llbox):
-        pass
+    # @abstractmethod
+    # def get_void_mass(self, void_box, llbox):
+    #     pass
