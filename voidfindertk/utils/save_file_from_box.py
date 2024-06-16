0cc7b56278e097266080cd047f63106864fdfc46
jorgefederico <jorgefedericobustillosrava@gmail.com>
2024-06-16 17:11:34 -0300

    feat:adding common modules

---
+++ voidfindertk/utils/save_file_from_box.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+def box_to_csv(box,file_path,**kwargs):
+    kwargs.setdefault('path_or_buf',file_path)
+    kwargs.setdefault('sep','\s+')
+    kwargs.setdefault('header',False)
+    kwargs.setdefault('index',False)
+    df = pd.DataFrame(box.__dict__)
+    df.to_csv(**kwargs)
+
