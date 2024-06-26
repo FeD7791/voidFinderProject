e2e9e5101b83054f2854503f713fd0184b12920e
juanbc <jbcabral@unc.edu.ar>
2024-06-25 22:38:04 -0300

    repr

--- voidfindertk/voids.py
+++ voidfindertk/voids.py
@@ -2,42 +2,13 @@ import numpy as np
 
 
 class Voids:
-    """
-    A class representing voids and their associated tracers.
-
-    Parameters
-    ----------
-    tracers : iterable
-        List or array-like object containing tracers associated with voids.
-    voids : iterable
-        List or array-like object containing voids.
-
-    Attributes
-    ----------
-    tracers : Object
-        Box object that contains tracers and their properties.
-    voids : list
-        List of ZobovVoids objects.
-
-    Methods
-    -------
-    voids_numbers()
-        Returns the number of voids.
-    void_of(tracer)
-        Finds and returns indices of voids containing a specific tracer.
-
-    Notes
-    -----
-    This class inherits from ModelVoid.
-
-    """
 
     def __init__(self, *, method, tracers, voids, extra):
         if len(tracers) <= len(voids):
             raise ValueError()
 
         self._method = str(method)
-        self._tracers = tracers  # the tracers
+        self._tracers = tracers.copy()  # the tracers
         self._voids = tuple(voids)  # tuple of arrays
         self._extra = dict(extra)  # dict with zaraza
 
@@ -63,7 +34,10 @@ class Voids:
 
     # REPR ====================================================================
     def __repr__(self):
-        return ""
+        return (
+            f"<Voids '{self.method}' "
+            f"{self.numbers_of_voids}V, {len(self.tracers)}T>"
+        )
 
     # utilities ===============================================================
 
