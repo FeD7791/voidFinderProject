d00ffcfbba27626873af8da806a20674f1ac9d9c
JBC <jbcabral@unc.edu.ar>
2024-07-03 13:06:12 -0300

    bunch in extra

--- voidfindertk/dive/__init__.py
+++ voidfindertk/dive/__init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Void-Finder-Toolkit Project (https://github.com/FeD7791/voidFinderProject).
+# Copyright (c) 2023-2024, Bustillos, J.F.; Cabral, Juan
+# License: BSD 3-Clause
+# Full Text:
+#   https://raw.githubusercontent.com/FeD7791/voidFinderProject/dev/LICENSE.txt
+
 """DIVE: Detection, Identificacion and Voids Examination
 
 DIVE is a reimplementation of VIDE void detection algoritm in pure Python on
--- voidfindertk/utils/__init__.py
+++ voidfindertk/utils/__init__.py
@@ -1,4 +1,5 @@
 from .context_managers import chdir
+from .bunch import Bunch
 
 
-__all__ = ["chdir"]
+__all__ = ["chdir", "Bunch"]
