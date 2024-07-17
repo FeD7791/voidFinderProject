80ab41180a29baab58ae69627418c6247dcb338a
jorgefederico <jorgefedericobustillosrava@gmail.com>
2024-07-16 22:37:20 -0300

    changing name from popcorn to svf_popcorn

--- voidfindertk/popcorn/popcorn_void_finder/Source/io_lib/io_tracers_stream.h
+++
@@ -1,9 +0,0 @@
-#ifndef IO_TRACERS_STREAM_H
-#define IO_TRACERS_STREAM_H
-
-#include <string>
-
-void ReadTracers_stream(std::string fname);
-void ReadHeaderTracers_stream(std::string fname);
-
-#endif
--- voidfindertk/popcorn/popcorn_void_finder/Source/popcorn.h
+++
@@ -1,9 +0,0 @@
-#ifndef POPCORN_H_
-#define POPCORN_H_
-
-#include "grid.h"
-
-int popcorn_start(float min_radius, double eps, float limit, int rank, int numrank);
-void popcorn_work(int cnt, int nshell, float min_radius, double eps, float limit, grid::Grid *G);
-
-#endif
