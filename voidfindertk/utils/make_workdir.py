29e72498c54e6a57c319b80669b15ed24af55308
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-08-03 18:09:51 -0300

    refactor: renaming make workdir maker

---
+++ voidfindertk/utils/make_workdir.py
@@ -0,0 +1,20 @@
+import pathlib
+import tempfile
+import datetime as dt
+
+
+def create_run_work_dir(*, workdir_path):
+    """
+    This method will create a temporal directory inside the working
+    directory of the ZobovVF class workdir.
+
+    Returns
+    -------
+        run_work_dir: pathlib.Path
+            path of the work directoty
+    """
+    timestamp = dt.datetime.now(dt.timezone.utc).isoformat()
+    run_work_dir = pathlib.Path(
+        tempfile.mkdtemp(suffix=timestamp, dir=workdir_path)
+    )
+    return run_work_dir
