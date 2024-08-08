ddaffd039f007144ba70f8d5e294bd5bc25f2a54
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-08-08 19:07:14 -0300

    feat: adding __init__ file to svf_popcorn

---
+++ voidfindertk/svf_popcorn/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Void-Finder-Toolkit Project (https://github.com/FeD7791/voidFinderProject).
+# Copyright (c) 2023-2024, Bustillos, J.F.; Cabral, Juan
+# License: BSD 3-Clause
+# Full Text:
+#   https://raw.githubusercontent.com/FeD7791/voidFinderProject/dev/LICENSE.txt
+
+from ._svf_popcorn import PopCornVF
+
+__all__ = ["PopCornVF"]
