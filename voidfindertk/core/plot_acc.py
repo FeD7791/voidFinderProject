ceeffd10a77634e7e7a7ba84649e1bb663936ee2
JBC <jbcabral@unc.edu.ar>
2024-09-12 18:16:57 -0300

    plots now voids is an attrs class

---
+++ voidfindertk/core/plot_acc.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import seaborn as sns
+
+from ..utils import accabc
+
+
+class BoxPlotter(accabc.AccessorABC):
+    """Plotter: Plotter object that plots the boxes."""
+
+    _default_kind = "hist2d"
+
+    def __init__(self, box):
+        self._box = box
+
+    def hist2d(self, x, y, *, ax=None, **kwargs):
+        ax = plt.gca() if ax is None else ax
+
+        xvalues = getattr(self._box, x)
+        yvalues = getattr(self._box, y)
+
+        sns.histplot(x=xvalues, y=yvalues, ax=ax, **kwargs)
+
+        ax.set_xlabel(f"{x} ({xvalues.unit})")
+        ax.set_ylabel(f"{y} ({yvalues.unit})")
+
+        return ax
+
+
+class VoidPlotter(accabc.AccessorABC):
+    """Plotter: Plotter object that plots the voids."""
+
+    _default_kind = "void_size_function"
+
+    def __init__(self, voids):
+        self._voids = voids
+
+    def __getattr__(self, kind):
+        voids = self._voids
+        return getattr(voids.box.plot, kind)
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+    def void_size_function(self, *, ax=None, vsf_kws=None, **kwargs):
+        # confs
+        vsf_kws = {} if vsf_kws is None else vsf_kws
+
+        # if no axis get the default
+        ax = plt.gca() if ax is None else ax
+
+        # get the vsf
+        x, y, delta = self._voids.vsf(**vsf_kws)
+
+        kwargs.setdefault("marker", "o")
+        kwargs.setdefault("label", "Void Size Function")
+        sns.lineplot(x=x, y=y, ax=ax, **kwargs)
+
+        ax.set_xlabel(r"$log_{10}(R)$" f"{x.unit}")
+        ax.set_ylabel(r"$\frac{1}{V} \frac{dN_v}{dlnR_v}$")
+
+        ax.set_yscale("log")
+
+        ax.set_title(f"Void Size Function\nSvdW Density Contrast {delta}")
+
+        ax.grid(True)
+
+        return ax
+
+    vsf = void_size_function
+
+
+
+
