ca769286e20cb860c7965ecfe44893127e8a448c
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-09-23 16:54:00 -0300

    updating scripts

--- draft/scripts/script_try_dive.py
+++ draft/scripts/script_try_dive.py
@@ -1,30 +1,64 @@
 from voidfindertk import io
-from voidfindertk import dive
+from voidfindertk import dive,zobov
+from voidfindertk import core
+import joblib
 import time
 import pathlib
 
 # Run full finder
 
-path = "/home/jorgefederico/updates/vftk_actual002/voidFinderProject/datasets/halos_ascii_1000_1024_npmin_10_z0.51.dat"
-# path = "/home/jorgefederico/updates/vftk_actual002/voidFinderProject/datasets/halo_catalogue.txt"
+dataset_path = pathlib.Path("/home/jorgefederico/updates/vftk_1109/voidFinderProject/datasets/")
+
 # Get Box
-dbox = io.xyz_read_table(path, usecols=[1,2,3])
-box = dbox.box
+box = io.read_table(dataset_path/"halos_ascii_1000_1024_npmin_10_z0.51.dat",names=["m","x", "y", "z", "vx", "vy", "vz"])
 
-workdir = "/home/jorgefederico/updates/vftk_actual002/voidFinderProject/runv"
 
-# model
-model = dive.DiveVF(box_size=1000, workdir=workdir, delta_r=[0.,50.], overlap_criterion=True, ratio=0.1)
-# start_time = time.time()
-# voids = model.find(databox=dbox)
-# print("--- %s seconds ---" % (time.time() - start_time))
+workdir = pathlib.Path("/home/jorgefederico/updates/vftk_1109/voidFinderProject/runz")
 
-## Just build voids
-model_find_parameters = {
-    'run_work_dir':pathlib.Path(workdir) / 'tmpluh7pb6n2024-08-09T19:14:07.847469+00:00',
-    'box':box
-                         }
+def build_zobov_model(workdir,*,delta=0.2):
 
-start_time = time.time()
-v1,v2,v3 = model.build_voids(model_find_parameters=model_find_parameters)
-print("--- %s seconds ---" % (time.time() - start_time))
+    model = zobov.ZobovVF(
+        density_threshold=0.2, #zobov parameter
+        box_size=1000,
+        workdir=workdir
+    )
+    model_find_parameters = {
+    'run_work_dir':model.workdir / "tmpuo2jislx2024-09-18T23:23:15.845781+00:00",
+    'box':box}
+    tinv,centers,extra = model.build_voids(model_find_parameters)
+    void = core.Voids(
+    method="ZOBOV",
+    box=box,
+    tracers_in_voids_ = tinv,
+    centers_=centers,
+    extra_= extra
+    )
+    joblib.dump(void, f"zobov_model_{delta}.pkl")
+
+
+
+def build_dive_model(box,workdir,*,threshold=0.2,op_crit=False,ratio=1.5):
+    # model
+    dive_model = dive.DiveVF(
+        density_threshold=0.2, #zobov parameter
+        box_size=1000, 
+        workdir=workdir, 
+        delta_r=[0.0,100.], 
+        threshold=threshold, #This would be a density contrast of -0.8
+        overlap_criterion=op_crit, #False: The void with the higher central density is rejected
+        ratio=ratio)
+    
+    model_find_parameters = {
+    'run_work_dir':dive_model.workdir / "tmpuo2jislx2024-09-18T23:23:15.845781+00:00",   #"tmpv4suzrwr2024-09-16T00:16:06.078226+00:00",
+    'box':box}
+    tinv,centers,extra = dive_model.build_voids(model_find_parameters)
+    void = core.Voids(
+    method="Dive",
+    box=box,
+    tracers_in_voids_ = tinv,
+    centers_=centers,
+    extra_= extra
+    )
+    joblib.dump(void, f"dive_model_t_{threshold}_rat_{ratio}.pkl")
+
+
--- draft/scripts/script_try_real_popcorn.py
+++ draft/scripts/script_try_real_popcorn.py
@@ -1,28 +1,214 @@
-from voidfindertk import io, popcorn
+from voidfindertk import io, popcorn, svf_popcorn, core
 import os
 import pathlib
-import time
+import matplotlib.pyplot as plt
+import joblib
 
 dataset_path = pathlib.Path("./datasets")
 workdir_path = pathlib.Path(os.path.abspath("./run_real_popcorn"))
 
 dbox = io.read_table(dataset_path/"halos_ascii_1000_1024_npmin_10_z0.51.dat",names=["m","x", "y", "z", "vx", "vy", "vz"])
 
-model = popcorn.PopCorn(
+
+
+
+# void09 = model_09.find(databox=dbox)
+# void08 = model_08.find(databox=dbox)
+# void07 = model_07.find(databox=dbox)
+
+#solo hacer ultima parte
+## Estudio radio de shot noise
+#model07
+model07 = svf_popcorn.SVFPopCorn(
     workdir=workdir_path,
     boxsize=1000,
-    densth=-0.9,
-    minradius = 20,
+    densth=-0.7,
+    minradius = 10,
     maxradius = 100,
-    shot_noise_threshold = 25
 )
-void = model.find(databox=dbox)
+tracers07,centers07,extra07 = model07.build_voids({
+    "run_work_dir": model07.workdir / "tmplxhn4wcw2024-08-22T01:08:51.465883+00:00",
+    "box": dbox.box})
+voids07 = core.Voids(
+    method='svf_popcorn',
+    box = dbox.box,
+    tracers_in_voids_ = tracers07,
+    centers_ = centers07,
+    extra_ = extra07
+)
+joblib.dump(voids07, "svf_popcorn_model_07.pkl")
+plot07 = core.VoidPlotter(
+    voids=voids07
+)
+vsf_kws={'delta':-0.7,'n_neighbors':600,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+ax7 = plot07.void_size_function(vsf_kws=vsf_kws)
+
+# fig2 = plt.figure()
+# fig2.axes.append(ax7)
+
+# fig2.savefig("vsf07.jpg")
+ax7.figure.savefig("svf_popcorn_07.jpg")
+fig,ax = plt.subplots()
+#### FIn estudio
 
 
 
-# read_and_modify_config(
-#     config_file_path="./vars.conf",
-#     section="INPUT_PARAMS",
-#     parameter="MINRADIUS",
-#     new_value=str(57)
-#     )
+
+
+
+#model08
+# model08 = svf_popcorn.SVFPopCorn(
+#     workdir=workdir_path,
+#     boxsize=1000,
+#     densth=-0.8,
+#     minradius = 10,
+#     maxradius = 100,
+# )
+# tracers08,centers08,extra08 = model08.build_voids({
+#     "run_work_dir": model08.workdir / "tmpbxqny8ro2024-09-14T23:59:51.396390+00:00",
+#     "box": dbox.box})
+# voids08 = core.Voids(
+#     method='svf_popcorn',
+#     box = dbox.box,
+#     tracers_in_voids_ = tracers08,
+#     centers_ = centers08,
+#     extra_ = extra08
+# )
+# joblib.dump(voids08, "svf_popcorn_model_08.pkl")
+# plot08 = core.VoidPlotter(
+#     voids=voids08
+# )
+# vsf_kws={'delta':-0.8,'n_neighbors':400,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+# ax8 = plot08.void_size_function(vsf_kws=vsf_kws)
+# ax8.figure.savefig("vsf_popcorn_08.jpg")
+
+# fig2 = plt.figure()
+# fig2.axes.append(ax8)
+# plt.savefig("vsf08.jpg")
+# fig2.savefig("vsf08.jpg")
+#### FIn estudio
+
+
+#model09 for svf popcron
+# model09 = svf_popcorn.SVFPopCorn(
+#     workdir=workdir_path,
+#     boxsize=1000,
+#     densth=-0.9,
+#     minradius = 10,
+#     maxradius = 100,
+# )
+# tracers,centers,extra = model09.build_voids({
+#     "run_work_dir": model09.workdir / "tmpipk1m12i2024-09-14T23:29:54.863443+00:00",
+#     "box": dbox.box})
+# voids = core.Voids(
+#     method='svf_popcorn',
+#     box = dbox.box,
+#     tracers_in_voids_ = tracers,
+#     centers_ = centers,
+#     extra_ = extra
+# )
+# joblib.dump(voids, "svf_popcorn_model_09.pkl")
+# plot09 = core.VoidPlotter(voids=voids)
+# vsf_kws={'delta':-0.9,'n_neighbors':400,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+# ax = plot09.void_size_function(vsf_kws=vsf_kws)
+# ax.figure.savefig("svf_popcorn_09.jpg")
+# fig,ax = plt.subplots()
+# plot = core.VoidPlotter(
+#     voids=voids
+# )
+# ax = plot.void_size_function()
+# ax.figure.savefig("vsf09.jpg")
+# #### Fin estudio
+
+# model_09 = popcorn.PopCorn(
+#     workdir=workdir_path,
+#     boxsize=1000,
+#     densth=-0.9,
+#     minradius = 10,
+#     maxradius = 100,
+#     shot_noise_threshold = 19
+# )
+
+
+
+# tracers09,centers09,extra09 = model_09.build_voids({
+#     "run_work_dir": model_09.workdir / "tmpipk1m12i2024-09-14T23:29:54.863443+00:00",
+#     "box": dbox.box})
+
+# voids = core.Voids(
+#     method='real_popcorn',
+#     box = dbox.box,
+#     tracers_in_voids_ = tracers09,
+#     centers_ = centers09,
+#     extra_ = extra09
+# )
+# joblib.dump(voids, "real_popcorn_model_09.pkl")
+# # get effetive radius
+# # effective_rad = voids.effective_radius()
+# plot09 = core.VoidPlotter(voids=voids)
+# vsf_kws={'delta':-0.9,'n_neighbors':400,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+# ax = plot09.void_size_function(vsf_kws=vsf_kws)
+# ax.figure.savefig("real_popcorn_999.jpg")
+# fig,ax = plt.subplots()
+
+
+
+# model_08 = popcorn.PopCorn(
+#     workdir=workdir_path,
+#     boxsize=1000,
+#     densth=-0.8,
+#     minradius = 10,
+#     maxradius = 100,
+#     shot_noise_threshold = 20
+# )
+
+# tracers08,centers08,extra08 = model_08.build_voids({
+#     "run_work_dir": model_08.workdir / "tmpbxqny8ro2024-09-14T23:59:51.396390+00:00",
+#     "box": dbox.box})
+
+# voids_real_08 = core.Voids(
+#     method='real_popcorn',
+#     box = dbox.box,
+#     tracers_in_voids_ = tracers08,
+#     centers_ = centers08,
+#     extra_ = extra08
+# )
+# joblib.dump(voids_real_08, "real_popcorn_model_08.pkl")
+
+# plot08 = core.VoidPlotter(voids=voids_real_08)
+# vsf_kws={'delta':-0.8,'n_neighbors':400,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+# ax = plot08.void_size_function(vsf_kws=vsf_kws)
+# ax.figure.savefig("real_popcorn_08.jpg")
+# fig,ax = plt.subplots()
+
+
+
+
+
+
+model_07 = popcorn.PopCorn(
+    workdir=workdir_path,
+    boxsize=1000,
+    densth=-0.7,
+    minradius = 10,
+    maxradius = 100,
+    shot_noise_threshold = 19.7
+)
+
+tracers07,centers07,extra07  = model_07.build_voids({
+    "run_work_dir": model_07.workdir / "tmplxhn4wcw2024-08-22T01:08:51.465883+00:00",
+    "box": dbox.box})
+
+voids07 = core.Voids(
+    method='real_popcorn',
+    box = dbox.box,
+    tracers_in_voids_ = tracers07,
+    centers_ = centers07,
+    extra_ = extra07
+)
+joblib.dump(voids07, "real_popcorn_model_07.pkl")
+plot08 = core.VoidPlotter(voids=voids07)
+vsf_kws={'delta':-0.7,'n_neighbors':600,'n_step1':1,'n_step2':3,'scale_1_num_samples':8,'scale_2_num_samples':1}
+ax = plot08.void_size_function(vsf_kws=vsf_kws)
+ax.figure.savefig("real_popcorn_07.jpg")
+fig,ax = plt.subplots()
