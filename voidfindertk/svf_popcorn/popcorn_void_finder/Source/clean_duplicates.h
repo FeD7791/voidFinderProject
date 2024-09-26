962e7a089f632db48f8b05b446830eeeb4a16634
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-09-26 07:54:54 -0300

    refactor: changing and deleting directory(s) for popcorn

--- voidfindertk/svf_popcorn/popcorn_void_finder/Source/clean_duplicates.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLEAN_DUPLICATES_H_
-#define CLEAN_DUPLICATES_H_
-
-#include <vector>
-
-int clean_duplicates_start(float overlap, std::vector<bool> *erase);
-
-#endif
--- voidfindertk/svf_popcorn/popcorn_void_finder/Source/compute_intersecs.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMPUTE_INTERSECS_H_
-#define COMPUTE_INTERSECS_H_
-
-int compute_intersecs_start();
-
-#endif
