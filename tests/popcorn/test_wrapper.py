26632b5a23cdfc47e216fab532ea97c1fbe4897f
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-10-14 19:22:46 -0300

    test: adding headers

---
+++ tests/popcorn/test_wrapper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 - 2024, Bustillos Federico, Gualpa Sebastian, Cabral Juan,
+#                            Paz Dante, Ruiz Andres, Correa Carlos
+# License: MIT
+# Full Text: https://github.com/FeD7791/voidFinderProject/blob/dev/LICENSE.txt
+# All rights reserved.
--- tests/zobov/test_wrapper.py
+++ tests/zobov/test_wrapper.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 - 2024, Bustillos Federico, Gualpa Sebastian, Cabral Juan,
+#                            Paz Dante, Ruiz Andres, Correa Carlos
+# License: MIT
+# Full Text: https://github.com/FeD7791/voidFinderProject/blob/dev/LICENSE.txt
+# All rights reserved.
 from unittest import mock
 
 from voidfindertk.zobov import _wrapper
