ca8e8513b1db72f2e6a4becdb86904699da80f88
FeD7791 <jorgefedericobustillosrava@gmail.com>
2024-11-04 16:03:22 -0300

    changes

---
+++ tests/datasets/test_spherical_cloud.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 - 2024, Bustillos Federico, Gualpa Sebastian, Cabral Juan,
+#                            Paz Dante, Ruiz Andres, Correa Carlos
+# License: MIT
+# Full Text: https://github.com/FeD7791/voidFinderProject/blob/dev/LICENSE.txt
+# All rights reserved.
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import grispy as gsp
+
+import numpy as np
+
+from voidfindertk.datasets import spherical_cloud
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+
+def test_cloud_with_overdensities():
+    lmax = 1000
+    rad = 30
+    delta = 2
+    cloud = spherical_cloud.build_cloud(lmax=lmax)
+    # Build centers
+    xyz_ = np.arange(0.0, lmax, rad + 10)
+    centers = np.column_stack((xyz_, xyz_, xyz_))
+    # Build cloud with overdensities
+    c_w_o = spherical_cloud.build_spherical_overdensity(
+        delta=delta, centers=centers, radii=rad, cloud=cloud
+    )
+    # Build grid
+    periodic = {0: (0, lmax), 1: (0, lmax), 2: (0, lmax)}
+    grid = gsp.GriSPy(c_w_o)
+    grid.set_periodicity(periodic, inplace=True)
+    # Perform buble search
+    dist_, indx = grid.bubble_neighbors(centers, distance_upper_bound=rad)
+    # Get mean density of universe
+    rho_med = c_w_o.shape[0] / lmax**3
+
+    number_tracers_in_voids = np.array(list(map(len, indx)))
+    # densities
+    dens = number_tracers_in_voids / ((4 / 3) * np.pi * rad**3)
+    # Ideal density
+    id_dens = rho_med * (delta + 1) * np.ones(len(dens))
+    assert np.allclose(dens, id_dens, rtol=0.5)
---
+++ voidfindertk/datasets/spherical_cloud.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 - 2024, Bustillos Federico, Gualpa Sebastian, Cabral Juan,
+#                            Paz Dante, Ruiz Andres, Correa Carlos
+# License: MIT
+# Full Text: https://github.com/FeD7791/voidFinderProject/blob/dev/LICENSE.txt
+# All rights reserved.
+
+"""Cloud with voids builder utility."""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import grispy as gsp
+
+import numpy as np
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+
+def _dens_gen(*, seed=2, center, rad, n_points):
+    """
+    Generate random points uniformly distributed within a spherical volume.
+
+    Parameters
+    ----------
+    seed : int, optional
+        Seed for the random number generator. Default is 2.
+    center : array_like, shape (3,)
+        The x, y, z coordinates of the center of the spherical volume.
+    rad : float
+        The radius of the spherical volume within which the points will be
+        generated.
+    n_points : int
+        The number of random points to generate.
+
+    Returns
+    -------
+    xyz : ndarray, shape (3, n_points)
+        An array of shape (3, n_points) where each column represents the
+        x, y, z coordinates of a generated point.
+
+    Notes
+    -----
+    The generated points are uniformly distributed in a spherical shell with
+    the specified radius and centered at the provided coordinates.
+
+    """
+    # generate_random_tracers_on_spherical_volume
+    rng = np.random.default_rng(seed=seed)
+    theta = rng.uniform(0, 2 * np.pi, size=(n_points, 1))
+    phi = rng.uniform(0, np.pi, size=(n_points, 1))
+    radii = rng.uniform(0, rad, size=(n_points, 1))
+    x = np.ravel(center[0] + radii * np.sin(theta) * np.cos(phi))
+    y = np.ravel(center[1] + radii * np.sin(theta) * np.sin(phi))
+    z = np.ravel(center[2] + radii * np.cos(theta))
+    xyz = np.column_stack((x, y, z))
+    return xyz
+
+
+def build_cloud(*, seed=2, lmin=0, lmax=1000, n_points=100**3):
+    """
+    Builds a cloud of n_points , where: lmin < x,y,z < lmax.
+
+    Parameters
+    ----------
+        seed: int
+            Seed for random number generator.
+        lmin: float
+            Low limit of random generated points.
+        lmax: float
+            Max limit of random generated points.
+        n_points: int
+            Number of points to be generated
+
+    Returns
+    -------
+        cloud: array
+            Array of n_points with x,y,z coordinates between lmin and lmax.
+
+    """
+    rng = np.random.default_rng(seed=seed)
+    # Create point cloud
+    cloud = rng.uniform(lmin, lmax, size=(n_points, 3))
+    return cloud
+
+
+def build_spherical_void(
+    delta,
+    centers: np.ndarray,
+    radii: float,
+    cloud,
+):
+    """
+    Takes a cloud of tracers and removes tracers inside an spherical shell \
+    around each void center so that each void has a desired density \
+    density contrast.
+
+    Parameters
+    ----------
+        delta: float
+            Integrated density contrast of the void. -1 < delta < 0
+        centers: array
+            Array of x,y,z positions of void centers.
+        radii: float
+            Radius of void. (All void have same radius)
+        cloud: array
+            Collection of tracers, each with positions x,y,z that constitu-
+            te the universe (Box)
+
+    Returns
+    -------
+        cloud_with_voids : array
+            Collection of tracers of cloud input, minus some tracers around
+            each void so that these have the desired density contrast.
+    """
+    cloud_volume = round(np.max(cloud) - np.min(cloud)) ** 3
+    cloud_density = len(cloud) / cloud_volume
+    density_voids = (1 + delta) * cloud_density
+
+    # Build Grid
+    grid = gsp.GriSPy(cloud)
+    # Set periodicity
+    periodic = {
+        0: (np.round(np.min(cloud), 0), np.round(np.max(cloud), 0)),
+        1: (np.round(np.min(cloud), 0), np.round(np.max(cloud), 0)),
+        2: (np.round(np.min(cloud), 0), np.round(np.max(cloud), 0)),
+    }
+    grid.set_periodicity(periodic, inplace=True)
+
+    # Find nearest neighbors
+    dist, index = grid.bubble_neighbors(centers, distance_upper_bound=radii)
+
+    # Calculate right number of tracers so the void gets at the desired
+    # density
+    n = int(round(density_voids * (4 / 3) * np.pi * (radii**3), 0))
+
+    new_index = [i[: len(i) - int(n - 1)] for i in index]
+    # dens_voids
+
+    # Remove from cloud the indicated indexes
+    mask = np.ones(len(cloud), dtype=bool)
+    for i in new_index:
+        mask[i] = False
+
+    cloud_with_voids = cloud[mask]
+    # Check the voids have the correct density
+    dens_validator(
+        cloud_with_voids=cloud_with_voids,
+        centers_of_voids=centers,
+        radius=radii,
+        density=density_voids,
+    )
+    return cloud_with_voids
+
+
+def build_spherical_overdensity(delta, centers, radii, cloud):
+    """
+    Build a new point cloud by adding tracers to regions of specified centers \
+    with a desired overdensity.
+
+    Parameters
+    ----------
+    delta : float
+        The overdensity factor to apply to the mean density of the cloud.
+    centers : array_like, shape (n_centers, 3)
+        An array of shape (n_centers, 3) containing the x, y, z coordinates
+        of the centers where overdensity should be applied.
+    radii : array_like, shape (n_centers,)
+        An array of shape (n_centers,) specifying the radii of the spherical
+        regions around each center where tracers will be added.
+    cloud : array_like, shape (n_points, 3)
+        The initial point cloud represented as an array of shape (n_points, 3),
+        where each row corresponds to the x, y, z coordinates of a point.
+
+    Returns
+    -------
+    cloud_with_overdensities : ndarray, shape (n_points + n_added_points, 3)
+        A new array containing the original points in `cloud` along with
+        additional tracers added to achieve the specified overdensity in
+        the defined spherical regions.
+
+    Notes
+    -----
+    This function assumes a periodic boundary condition for the point cloud
+    and uses the Grispy library to find neighboring points within the given
+    radii.
+
+    """
+    rho_med = len(cloud) / (np.max(cloud)) ** 3
+    rho = (1 + delta) * rho_med
+    # Get number of tracers that should be in void so that the overdensity is
+    # reached for a certain radii (Rounded to closest integer)
+    n_ideal_tracers_in_void = np.round(((4 / 3) * np.pi * radii**3) * rho, 0)
+
+    # Get number of tracers in void using Grispy
+    periodic = {
+        0: (np.min(cloud), np.max(cloud)),
+        1: (np.min(cloud), np.max(cloud)),
+        2: (np.min(cloud), np.max(cloud)),
+    }
+    grid = gsp.GriSPy(cloud)
+    grid.set_periodicity(periodic, inplace=True)
+    dist_, indx = grid.bubble_neighbors(centers, distance_upper_bound=radii)
+    n_actual_tracers_in_void = np.array(list(map(len, indx)))
+
+    # Calculate number of tracers that should be added
+    add_n = n_ideal_tracers_in_void - n_actual_tracers_in_void
+
+    # Calculate tracers xyz positions
+    new_tracers = [
+        _dens_gen(center=e[0], rad=radii, n_points=int(e[1]))
+        for e in zip(centers, add_n)
+    ]
+    new_tracers.append(cloud)
+    # Add tracers to cloud
+    cloud_with_overdensities = np.concatenate(new_tracers)
+    return cloud_with_overdensities
+
+
+# =============================================================================
+# OTHER
+# =============================================================================
+def dens_validator(*, cloud_with_voids, centers_of_voids, radius, density):
+    """Validator for build_spherical_void. Checks if the created cloud with \
+    voids has the desired density contrast for each void."""
+    # Build Grid
+    grid = gsp.GriSPy(cloud_with_voids)
+    # Add periodicity
+    periodic = {
+        0: (np.min(cloud_with_voids), np.max(cloud_with_voids)),
+        1: (np.min(cloud_with_voids), np.max(cloud_with_voids)),
+        2: (np.min(cloud_with_voids), np.max(cloud_with_voids)),
+    }
+    grid.set_periodicity(periodic, inplace=True)
+    # Perform buble search of neighbors around center
+    distances, tracers = grid.bubble_neighbors(centers_of_voids, radius)
+    # Calculate densities
+    densities = np.array(list(map(len, tracers))) / (
+        (4 / 3) * np.pi * radius**3
+    )
+    # Check densities are the desired density
+    check_dens = np.all(
+        np.less_equal(densities, density * np.ones(densities.shape))
+    )
+    if not check_dens:
+        raise ValueError("Wrong density of voids")
